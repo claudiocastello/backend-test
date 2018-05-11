5177b7abb3ac3e4eb5e8012d3594a84ee13b3318
claudiocastello <claudio.chemist@gmail.com>
2018-05-10 22:53:22 -0300

    Almost finished api; playing around with test_api and some frontend.

--- api_test/views.py
+++ api_test/views.py
@@ -1,4 +1,4 @@
-import requests as python_requests
+import requests as python_requests, json
 
 from flask import abort, render_template, redirect, url_for, flash, request as flask_request
 
@@ -7,7 +7,6 @@ from .forms import TestAPIForm
 
 
 ### Views ###
-
 @app.route('/', methods=['GET', 'POST'])
 def search():
     # Instantiate the form object
@@ -18,6 +17,8 @@ def search():
     cities_url = 'http://localhost:5000/catho/api/v1.0/cities'
     city_list = python_requests.get(cities_url).json()
     form.cities.choices = [(city, city) for city in city_list]
+
+    search_results = []
 
     if form.validate_on_submit():
         if flask_request.method == 'POST':
@@ -31,7 +32,14 @@ def search():
                         'city': city,
                         'order': order
                         }
-            print(form_data)
+
             query_request = python_requests.get(url=URL, params=form_data)
-            return redirect(query_request.url)
-    return render_template('index.html', form=form, cities=city_list)
+            search_results = query_request.json()
+            return render_template('index.html',
+                                    form=form,
+                                    cities=city_list,
+                                    search_results=search_results)
+    return render_template('index.html',
+                            form=form,
+                            cities=city_list,
+                            search_results=search_results)
